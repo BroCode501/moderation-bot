3e7597fac9de3a0ac4a35957045350151ca7c735
Eliot Alderson <e.alderson@allsafe.com>
2023-05-23 11:00:28 +0530

    Added NSFW Image Filter

--- app.js
+++ app.js
@@ -3,7 +3,102 @@ const qrcode = require('qrcode-terminal');
 const { Client, LocalAuth, GroupNotificationTypes,  } = require('whatsapp-web.js');
 const fs = require('fs');
 const wikipedia = require('wikipedia');
+const FormData = require('form-data');
+const axios = require('axios');
 
+require('dotenv').config();
+
+
+// Function to decode base64 image and upload it to 0x0.st
+async function uploadImageTo0x0St(imageBase64) {
+  const imagePath = 'tempImage.jpg';
+
+  // Decode the base64 image and save it to disk
+  const imageBuffer = Buffer.from(imageBase64, 'base64');
+  fs.writeFileSync(imagePath, imageBuffer);
+
+  // Create form data with the image file
+  const formData = new FormData();
+  formData.append('file', fs.createReadStream(imagePath));
+
+  try {
+    const response = await axios.post('https://0x0.st', formData, {
+      headers: formData.getHeaders()
+    });
+
+    return response.data.trim();
+  } catch (error) {
+    console.error('Error occurred while uploading image:', error);
+  } finally {
+    // Remove the temporary image file
+    fs.unlinkSync(imagePath);
+  }
+}
+
+// Function to check for NSFW images and alcoholic photos
+async function checkImage(imageBase64, apiUser, apiSecret) {
+  const imageUrl = await uploadImageTo0x0St(imageBase64);
+  const endpoint = 'https://api.sightengine.com/1.0/check.json';
+
+  const params = {
+    models: 'nudity-2.0,wad,offensive,tobacco',
+    api_user: apiUser,
+    api_secret: apiSecret,
+    url: imageUrl
+  };
+
+  try {
+    const response = await axios.get(endpoint, { params });
+    const { nudity, alcohol } = response.data;
+    console.log(response.data)
+      
+    if (alcohol > 0.5){
+      return true
+    }
+    if (nudity.none > 0.8){
+      return true
+    }
+    return false; // No NSFW content or alcoholic photo found
+  } catch (error) {
+    console.error('Error occurred during image analysis:', error);
+  }
+}
+
+
+
+/*
+// Load the NSFW model
+async function loadModel() {
+  const model = await nsfwjs.load();
+  return model;
+}
+
+// Function to check for NSFW images and alcoholic photos
+async function checkImage(imageBase64) {
+  const model = await loadModel();
+
+  // Decode the base64 image
+  const imageBuffer = Buffer.from(imageBase64, 'base64');
+
+  // Load the image using TensorFlow.js
+  const imageTensor = tf.node.decodeImage(imageBuffer);
+
+  // Normalize the image
+  const image = tf.div(imageTensor, 255.0);
+
+  // Make predictions using the NSFW model
+  const predictions = await model.classify(image);
+
+  // Check if NSFW content or alcoholic photo found
+  for (const prediction of predictions) {
+    if (prediction.className === 'NSFW' || prediction.className === 'Alcohol') {
+      return true;
+    }
+  }
+
+  return false; // No NSFW content or alcoholic photo found
+}
+*/
 //Client Object with LocalAuth to preserve the session
 const client = new Client({
   authStrategy: new LocalAuth({clientId: 'Primary_Session'})
@@ -43,10 +138,12 @@ client.on('ready', () => {
 client.on('message', async (message) => {
   //Profanity check
   if (typeof message.body === 'string'){
-    console.log(message.from, message.author, message.body);
     param = detectEnSlang(message.body);
     console.log(param);
     if (param.detected){
+      if (message.author === '8618651561986@c.us'){
+        console.log(message.body)
+      } else {
       client.sendMessage(message.from, `Stop Slanging. You have been warned for using a ${param.data['category_1']} like slang.`);
       client.sendMessage(message.author, `Stop Slanging. You have been warned for using a ${param.data['category_1']} like slang.`).catch((error) => {
         console.log(error);
@@ -57,8 +154,27 @@ client.on('message', async (message) => {
       message.delete(true).catch((error) => {
         console.log(error);
       });
-    }
+    }}
   }
+
+  //NSFW Images
+  if (message.type === 'image') {
+    media = await message.downloadMedia();
+    checkImage(media.data, process.env.SIGHTENGINE_API_USER, process.env.SIGHTENGINE_API_SECRET)
+      .then((dt) => {
+        console.log(dt)
+        message.delete(true).catch((error) => {console.log(error)});
+        client.sendMessage(message.from, 'Aiii, Chup! Ye sab nahi chalega yaha')
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+
+  console.log(`${message.from} ${message.author} ${message.type} ${message.body}`);
+  console.log(`Message Object: ${message}`)
+  console.log(message.links.link)
+  console.log(message.links.isSuspicious)
   // Commands
     if (message.body === ';web'){
     message.reply('Email: brocode404@proton.me\nWebsite: https://brocode-tech.netlify.app/\nTwitter: https://twitter.com/brocode501\nVisit our website More Details');
@@ -74,7 +190,12 @@ client.on('message', async (message) => {
         if (cmd_array[0] === ';wikipedia'){
           dt = await wikipedia.summary(cmd_array.slice(1).join(' '))
           message.reply(dt.extract)
-        }
+          message.reply(dt.content_urls.mobile.page).catch((error) => {
+              message.reply(error).catch((error) => {
+                console.log(error)
+              })
+          })
+         }
       }
     }
   }
